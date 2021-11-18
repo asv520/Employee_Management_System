7ac777b1d4c9e4b1b69a25fc4a53b34bb716f759
asv520 <iamasv520@gmail.com>
2021-11-18 10:19:34 +0530

    Add Employee class

---
+++ Employee.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class Employee {
+    String id, name, email, salary, contactNumber;
+
+    public Employee(String id, String name, String email, String salary, String contactNumber) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.salary = salary;
+        this.contactNumber = contactNumber;
+    }
+
+    public Employee(){
+    }
+    
+}
