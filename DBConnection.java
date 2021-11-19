8d4218175bdfc5fec5f4b72bbc281a3430484cb3
asv520 <iamasv520@gmail.com>
2021-11-19 22:46:13 +0530

    Add DBConnection class

---
+++ DBConnection.java
@@ -0,0 +1,11 @@
+package com.company;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+public class DBConnection {
+    public static Connection getConnection() throws ClassNotFoundException, SQLException {
+        Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/Details","root","Kronos@7");
+        return con;
+    }
+}
